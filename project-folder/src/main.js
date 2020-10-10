2e4242b7766338bca07137829fbce8888e76e663
ncmreis <ncmreis@gmail.com>
2020-10-10 20:26:08 +0100

    with router

--- project-folder/src/main.js
+++ project-folder/src/main.js
@@ -1,7 +1,42 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router';
+import vuetify from "./plugins/vuetify";
+
+import Home from './views/Home';
+import About from './views/About';
+import Services from './views/Services';
+import Strategy from './views/Strategy';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    component: Home,
+    name: 'home'
+  },
+  {
+    path: '/about',
+    component: About,
+    name: "about"
+  },
+  {
+    path: '/services',
+    component: Services,
+    name: "services"
+  },
+  {
+    path: '/services/strategy',
+    component: Strategy,
+    name: "strategy"
+  }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 Vue.config.productionTip = false;
 
---
+++ project-folder/src/views/Strategy.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Strategy"
+    }
+</script>
+
+<style scoped>
+
+</style>
